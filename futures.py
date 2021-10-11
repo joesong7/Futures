c752b7bd19842b682138cfada2e7d3ef85469f1d
宋鎔宇 <songrongyu@songrongyudeMacBook-Air.local>
2021-10-11 15:13:09 +0800

    匯出成csv功能

--- futures.py
+++ futures.py
@@ -1,13 +1,44 @@
 
 import requests
+import csv
 from bs4 import BeautifulSoup
 response = requests.get(
     "https://www.taifex.com.tw/cht/5/stockMargining")
 soup = BeautifulSoup(response.text, "html.parser")
 
-for item in soup.find_all('table','table_c'):
-     
-     print(item.text)
+table = soup.table
+table_rows=table.find_all('tr')
+output_rows=[]
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    output_rows.append(row)
+print(output_rows)
+
+
+response2 = requests.get(
+    "https://histock.tw/stock/future.aspx")
+soup2 = BeautifulSoup(response2.text, "html.parser")
+
+
+table2= soup2.find('table', {'class': 'gvTB mt10'})
+table_rows2=table2.find_all('tr')
+output_rows2=[]
+for tr in table_rows2:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    output_rows2.append(row)
+print(output_rows2)
+    
+    
+with open('myfile.csv', 'w', newline='') as file:
+    mywriter = csv.writer(file, delimiter=',')
+    mywriter.writerows(output_rows)
+with open('myfile2.csv', 'w', newline='') as file:
+    mywriter = csv.writer(file, delimiter=',')
+    mywriter.writerows(output_rows2)
+      
+
 
 
 
